498788559cd90c9fe745483bca70801076d4803b
TinOgrizek <ogrizek55@gmail.com>
2024-10-26 12:42:33 +0200

    Promjena vremenske zone
    Ispravak za upisivanje vremenske zone unutar baze podataka.

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const sequelize = new Sequelize(process.env.DB_URL, {
     dialect: "postgres",
     protocol: "postgres",
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
     dialectOptions: {
         ssl: {
             require: true,
